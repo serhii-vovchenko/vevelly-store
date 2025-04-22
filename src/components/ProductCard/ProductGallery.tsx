4087d922d8545e33bab99183581d15e807cc45cb
Serhii Vovchenko <serega.vovchenko@gmail.com>
2025-04-22 21:55:07 +0200

    fix gallery

--- src/components/ProductCard/ProductGallery.tsx
+++ src/components/ProductCard/ProductGallery.tsx
@@ -4,25 +4,25 @@ import { imagesSelector } from '../../redux/product/selectors';
 
 const ProductGallery = () => {
 	const images = useSelector(imagesSelector);
-	const [currentImage, setCurrentImage] = useState<string>(images?.length ? images[0] : '');
+	const [currentImage, setCurrentImage] = useState<number>(0);
 
-	const handleCurrentImage = (image: string): void => {
-		setCurrentImage(image);
+	const handleCurrentImage = (index: number): void => {
+		setCurrentImage(index);
 	};
 
 	return (
 		<div className="w-[683px] h-[424px] flex gap-5">
 			<ul className="flex flex-col gap-5 overflow-y-auto scrollbar-hide">
-				{images?.map(img => {
+				{images?.map((item, index) => {
 					return (
 						<li
-							key={img}
-							onClick={() => handleCurrentImage(img)}
+							key={index}
+							onClick={() => handleCurrentImage(index)}
 							className="w-[167px] h-[128px] flex flex-shrink-0 items-center justify-center border border-[#D6E8EE] overflow-hidden cursor-pointer"
 						>
 							<img
-								src={img}
-								alt={img}
+								src={item}
+								alt={item}
 								width="167"
 								height="128"
 								className="w-full h-full object-cover"
@@ -33,8 +33,8 @@ const ProductGallery = () => {
 			</ul>
 			<div className="w-[496px] h-[424px] border border-[#D6E8EE] overflow-hidden">
 				<img
-					src={currentImage}
-					alt={currentImage}
+					src={images && images[currentImage]}
+					alt={images && images[currentImage]}
 					width="496"
 					height="424"
 					className="w-full h-full object-cover"
